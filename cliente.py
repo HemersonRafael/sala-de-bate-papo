f2080157b3a8dde26aeebf44f5daac2ad6c63c6e
HemersonRafael <rafaelpontes1995@gmail.com>
2019-11-03 03:30:16 -0300

    start

--- cliente.py
+++ cliente.py
@@ -3,25 +3,23 @@
 # DISCIPLINA REDES DE COMPUTADORES (DCA0113)
 # AUTOR: PROF. CARLOS M D VIEGAS (viegas 'at' dca.ufrn.br)
 #
-# SCRIPT: Servidor de sockets TCP modificado para receber texto minusculo do cliente enviar resposta em maiuscula
+# SCRIPT: Cliente de sockets TCP modificado para enviar texto minusculo ao servidor e aguardar resposta em maiuscula
 #
 
 # importacao das bibliotecas
-from socket import * # sockets
+from socket import *
+import threading # threads
+
 
 # definicao das variaveis
-serverName = '' # ip do servidor (em branco)
-serverPort = 65000 # porta a se conectar
-serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
-serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
-serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
-print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
-while 1:
-  connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-  sentence = connectionSocket.recv(1024) # recebe dados do cliente
-  sentence = sentence.decode('utf-8')
-  capitalizedSentence = sentence.upper() # converte em letras maiusculas
-  print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
-  connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
-  connectionSocket.close() # encerra o socket com o cliente
-serverSocket.close() # encerra o socket do servidor
+serverName = 'localhost' # ip do servidor
+serverPort = 6500 # porta a se conectar
+clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
+while True:
+    sentence = input('Digite o texto em letras minusculas: ')
+    conteudo = str([len(sentence),'rafael','publica',sentence])
+    clientSocket.sendall(conteudo.encode('utf-8')) # envia o texto para o servidor
+    modifiedSentence = clientSocket.recv(1024) # recebe do servidor a resposta
+    print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
+clientSocket.close() # encerramento o socket do cliente
--- server.py
+++ server.py
@@ -8,20 +8,56 @@
 
 # importacao das bibliotecas
 from socket import * # sockets
+import threading # threads
+import time # tempo (opcional)
+clientes = {'0.0.0.0':'teste'}
+ips = ['0.0.0.0']
+def salve_cliente(connectionSocket, addr):
+    
+    if(ips.count(addr[0]) == 0):
+        ips.append(addr[0])
+        connectionSocket.send("informe o seu nickname".encode('utf-8'))
+        sentence = connectionSocket.recv(1024) # recebe dados do cliente
+        name = sentence.decode('utf-8')   
+        clientes.update({addr[0]:name})
+    else:
+        pass
+
+
+def to_receive(connectionSocket):
+    sentence = connectionSocket.recv(1024) # recebe dados do cliente
+    sentence = sentence.decode('utf-8')
+    capitalizedSentence = sentence.upper() # converte em letras maiusculas
+    print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
+    print(clientes)
+    connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
+    connectionSocket.close() # encerra o socket com o cliente
+
+# define uma classe para a criacao de threads
+class minhaThread (threading.Thread):
+    # redefine a funcao __init__ para aceitar a passagem parametros de entrada
+    def __init__(self, threadConnectionSocket):
+        threading.Thread.__init__(self)
+        self.connectionSocket = threadConnectionSocket
+       
+    # a funcao run() e executada por padrao por cada thread 
+    def run(self):
+        to_receive(self.connectionSocket)
+       
 
 # definicao das variaveis
-serverName = '' # ip do servidor (em branco)
-serverPort = 65000 # porta a se conectar
+serverName = '127.0.0.1' # ip do servidor (em branco)
+serverPort = 6500 # porta a se conectar
 serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
 serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
 print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
 while 1:
   connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-  sentence = connectionSocket.recv(1024) # recebe dados do cliente
-  sentence = sentence.decode('utf-8')
-  capitalizedSentence = sentence.upper() # converte em letras maiusculas
-  print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
-  connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
-  connectionSocket.close() # encerra o socket com o cliente
+  salve_cliente(connectionSocket, addr)
+  # criando threads
+  thread1 = minhaThread(connectionSocket)
+  # disparando as threads
+  thread1.start()
+  
 serverSocket.close() # encerra o socket do servidor
